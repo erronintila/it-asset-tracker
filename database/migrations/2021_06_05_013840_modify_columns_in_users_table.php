13bce3917fe6dc2ec124eb16d664f3ea0bb4aca4
Erron <erronintila@gmail.com>
2021-06-05 16:46:37 +0800

    updated database structure

--- database/migrations/2021_06_05_013840_modify_columns_in_users_table.php
+++ database/migrations/2021_06_05_013840_modify_columns_in_users_table.php
@@ -14,7 +14,20 @@ class ModifyColumnsInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string("company")->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('suffix')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->date("birthdate")->nullable();
+            $table->string("business_phone")->nullable();
+            $table->string("home_phone")->nullable();
+            $table->string("mobile_phone")->nullable();
+            $table->string("website")->nullable();
+            $table->string("fax")->nullable();
+            $table->string('username')->unique();
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +39,17 @@ class ModifyColumnsInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('first_name');
+            $table->dropColumn('middle_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('suffix');
+            $table->dropColumn('gender');
+            $table->dropColumn("birthdate");
+            $table->dropColumn("phone");
+            $table->dropColumn("website");
+            $table->dropColumn("fax");
+            $table->dropColumn("username");
+            $table->dropSoftDeletes();
         });
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,6 +15,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Administrator',
+            'username' => 'admin',
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
