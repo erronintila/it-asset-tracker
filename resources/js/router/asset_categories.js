440dff2d5cf185259866f2b914c2f6f1b51fb287
Erron Intila <erronintila@gmail.com>
2021-07-02 20:26:58 +0800

    Update Asset Category routes.

--- resources/js/router/asset_categories.js
+++ resources/js/router/asset_categories.js
@@ -1,6 +1,6 @@
 const asset_category_routes = [
     {
-        path: "/asset_categories",
+        path: "/settings/asset/categories",
         component: () => import("../pages/asset_categories/Index"),
         name: "asset_categories.index",
         meta: {
@@ -8,7 +8,7 @@ const asset_category_routes = [
         }
     },
     {
-        path: "/asset_categories/create",
+        path: "/settings/asset/categories/create",
         component: () => import("../pages/asset_categories/Create"),
         name: "asset_categories.create",
         meta: {
@@ -16,7 +16,7 @@ const asset_category_routes = [
         }
     },
     {
-        path: "/asset_categories/:id",
+        path: "/settings/asset/categories/:id",
         component: () => import("../pages/asset_categories/Show"),
         name: "asset_categories.show",
         meta: {
