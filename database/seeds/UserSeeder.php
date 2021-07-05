038a99f5e6de6445e5170a2f4b30d25546f58685
Erron Intila <erronintila@gmail.com>
2021-07-05 15:43:23 +0800

    Remove unused files and update UserSeeder.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,32 +13,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Administrator',
-            'username' => 'admin',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        // User::create([
+        //     'name' => 'Administrator',
+        //     'username' => 'admin',
+        //     'email' => 'admin@admin.com',
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
 
-            'company' => 'Twin-Circa Marketing',
-            'first_name' => 'Administrator',
-            'middle_name' => null,
-            'last_name' => '',
-            'suffix' => null,
-            'gender' => null,
-            'birthdate' => null,
-            'business_phone' => null,
-            'home_phone' => null,
-            'mobile_phone' => null,
-            'website' => null,
-            'fax' => null,
-            'username' => 'admin',
-            'job_title' => null,
-            'type' => 'admin',
-            'is_admin' => true,
-            'is_active' => true,
-        ]);
+        //     'company' => 'Twin-Circa Marketing',
+        //     'first_name' => 'Administrator',
+        //     'middle_name' => null,
+        //     'last_name' => '',
+        //     'suffix' => null,
+        //     'gender' => null,
+        //     'birthdate' => null,
+        //     'business_phone' => null,
+        //     'home_phone' => null,
+        //     'mobile_phone' => null,
+        //     'website' => null,
+        //     'fax' => null,
+        //     'username' => 'admin',
+        //     'job_title' => null,
+        //     'type' => 'admin',
+        //     'is_admin' => true,
+        //     'is_active' => true,
+        // ]);
 
         factory(\App\Models\User::class, 10)->create();
     }
